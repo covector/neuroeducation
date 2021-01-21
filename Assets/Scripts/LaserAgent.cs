16abf13e964c1affc530e0b2c5f363b0114286c1
covector <60296643+covector@users.noreply.github.com>
2021-01-21 16:57:00 +0800

    init scene

---
+++ Assets/Scripts/LaserAgent.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class LaserAgent : Agent
+{
+    public override void Initialize()
+    {
+
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Unity.MLAgents;
+
+public class Room : MonoBehaviour
+{
+    GameObject[] lasers;
+    float spawnTimer;
+    EnvironmentParameters m_ResetParams;
+    void Update()
+    {
+        
+    }
+    public void ResetRoom()
+    {
+
+    }
+    GameObject SpawnLaser()
+    {
+
+    }
+}
